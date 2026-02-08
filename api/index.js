7e82dd8e75575d5ea11fbba013f8e07e7543e344
laiwola <layiibrahim32@gmail.com>
2026-02-08 23:00:26 +0100

    vercel.json added

--- api/index.js
+++ api/index.js
@@ -17,7 +17,9 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
