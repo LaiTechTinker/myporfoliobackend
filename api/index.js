74517114ffcf823fb2b8af8084f488f3c03bb034
laiwola <layiibrahim32@gmail.com>
2026-02-09 15:26:09 +0100

    serverles modules changes

--- api/index.js
+++ api/index.js
@@ -80,8 +80,8 @@ app.use((req, res) => {
 
 export default serverless(app);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
